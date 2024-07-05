42468c42bd3f5ac4a8cf438935ccfc4ad2a0f08b
NicolasSokolowski <sokolowski.nicolas@gmail.com>
2024-07-05 17:33:29 +0200

    update

--- common/src/helpers/controllerWrapper.helper.ts
+++ common/src/helpers/controllerWrapper.helper.ts
@@ -1,11 +1,9 @@
 import { Request, Response, NextFunction } from 'express';
 
-const controllerWrapper = (controller: (req: Request, res: Response, next: NextFunction) => Promise<void>) => async (req: Request, res: Response, next: NextFunction) => {
+export const controllerWrapper = (controller: (req: Request, res: Response, next: NextFunction) => Promise<void>) => async (req: Request, res: Response, next: NextFunction) => {
   try {
     await controller(req, res, next);
   } catch (err) {
     next(err);
   }
 };
-
-export default controllerWrapper;
