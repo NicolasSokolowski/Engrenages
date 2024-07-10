4b46f7a41e59f30f4b015cd1ed581c0de0412822
NicolasSokolowski <sokolowski.nicolas@gmail.com>
2024-07-11 01:02:03 +0200

    todo tests

---
+++ structure/__test__/location.test.ts
@@ -0,0 +1,39 @@
+import request from "supertest";
+import { app } from "../app/index.app";
+import { makeRandomString } from "@zencorp/engrenages";
+
+// Helper functions ---
+
+// TESTS --------------
+
+test.todo("fetches all existing location");
+
+// --------------------
+
+test.todo("creates a location when given valid inputs");
+
+// --------------------
+
+test.todo("returns an error when given invalid inputs for the creation");
+
+// --------------------
+
+test.todo("creates and updates a location when given valid inpust");
+
+// --------------------
+
+test.todo("returns an error when given invalid inputs for the update");
+
+// --------------------
+
+test.todo("creates, updates a location several times and checks the version");
+
+// --------------------
+
+test.todo("deletes a location when given valid inputs");
+
+// --------------------
+
+test.todo("returns an error when given invalid inputs for the delete");
+
+// --------------------
---
+++ structure/__test__/location_type.test.ts
@@ -0,0 +1,39 @@
+import request from "supertest";
+import { app } from "../app/index.app";
+import { makeRandomString } from "@zencorp/engrenages";
+
+// Helper functions ---
+
+// TESTS --------------
+
+test.todo("fetches all existing location types");
+
+// --------------------
+
+test.todo("creates a location type when given valid inputs");
+
+// --------------------
+
+test.todo("returns an error when given invalid inputs for the creation");
+
+// --------------------
+
+test.todo("creates and updates a location when given valid inputs");
+
+// --------------------
+
+test.todo("returns an error when given invalid inputs for the update");
+
+// --------------------
+
+test.todo("creates, updates a location type several times and checks the version");
+
+// --------------------
+
+test.todo("deletes a location type when given valid inputs");
+
+// --------------------
+
+test.todo("returns an error when given invalid inputs for the delete");
+
+// --------------------
