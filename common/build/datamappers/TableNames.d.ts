71d0f6802d87afcda97f41f68fd8105a70157170
NicolasSokolowski <sokolowski.nicolas@gmail.com>
2024-07-13 23:35:23 +0200

    added role and user tableName to enum

--- common/build/datamappers/TableNames.d.ts
+++ common/build/datamappers/TableNames.d.ts
@@ -1,4 +1,6 @@
 export declare enum TableNames {
+    User = "user",
+    Role = "role",
     Product = "product",
     ProductBlockage = "product_blockage_code",
     Location = "location",
--- common/build/datamappers/TableNames.js
+++ common/build/datamappers/TableNames.js
@@ -3,6 +3,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.TableNames = void 0;
 var TableNames;
 (function (TableNames) {
+    TableNames["User"] = "user";
+    TableNames["Role"] = "role";
     TableNames["Product"] = "product";
     TableNames["ProductBlockage"] = "product_blockage_code";
     TableNames["Location"] = "location";
