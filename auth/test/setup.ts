e08fdb3228dec66c45cc4c844e567bf956dc52f5
NicolasSokolowski <sokolowski.nicolas@gmail.com>
2024-07-13 13:49:38 +0200

    build: test setup file

---
+++ auth/test/setup.ts
@@ -0,0 +1,27 @@
+import { exec } from 'child_process';
+
+let pool: any;
+
+beforeAll(async () => {
+});
+
+beforeEach(async () => {
+})
+
+afterAll(async () => {
+  if (pool) {
+    await pool.end();
+  }
+
+  await new Promise((resolve, reject) => {
+    exec("npm run db:reset", (error, stdout, stderr) => {
+      if (error) {
+        console.error(`Error executing db:reset: ${error}`);
+        reject(error);
+      } else {
+        if (stderr) console.error(`stderr: ${stderr}`);
+        resolve(stdout);
+      }
+    });
+  });
+});
