2d75fc2af63325f9c025fa306c32d2a485971cf0
NicolasSokolowski <sokolowski.nicolas@gmail.com>
2024-07-13 23:30:49 +0200

    build: role and user creation and update schemas

---
+++ auth/app/validation/index.schemas.ts
@@ -0,0 +1,13 @@
+import roleCreateSchema from "./schemas/role.create.schema";
+import roleUpdateSchema from "./schemas/role.update.schema";
+import userCreateSchema from "./schemas/user.create.schema";
+import userUpdateSchema from "./schemas/user.update.schema";
+import passwordUpdateSchema from "./schemas/password.update.schema";
+
+export {
+  roleCreateSchema,
+  roleUpdateSchema,
+  userCreateSchema,
+  userUpdateSchema,
+  passwordUpdateSchema
+};
