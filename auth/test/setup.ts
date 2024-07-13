7f2b6d4531dea0f45462edad6e47f8e7c5584c4d
NicolasSokolowski <sokolowski.nicolas@gmail.com>
2024-07-13 23:34:18 +0200

    config, pckge.json, docker ...

--- auth/test/setup.ts
+++ auth/test/setup.ts
@@ -6,6 +6,17 @@ beforeAll(async () => {
 });
 
 beforeEach(async () => {
+  await new Promise((resolve, reject) => {
+    exec("npm run db:reset", (error, stdout, stderr) => {
+      if (error) {
+        console.error(`Error executing db:reset: ${error}`);
+        reject(error);
+      } else {
+        if (stderr) console.error(`stderr: ${stderr}`);
+        resolve(stdout);
+      }
+    });
+  });
 })
 
 afterAll(async () => {
