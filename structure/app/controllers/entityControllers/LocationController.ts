f975e161a1773ff7be48a7a3a8d309e0d8ef3519
NicolasSokolowski <sokolowski.nicolas@gmail.com>
2024-07-26 01:40:04 +0200

    build: location controller & interface

---
+++ structure/app/controllers/entityControllers/LocationController.ts
@@ -0,0 +1,62 @@
+import { Request, Response } from "express";
+import { BadRequestError, CoreController, DatabaseConnectionError, NotFoundError } from "@zencorp/engrenages";
+import { LocationControllerRequirements } from "../interfaces/LocationControllerRequirements";
+import { LocationDatamapperRequirements } from "../../datamappers/interfaces/LocationDatamapperRequirements";
+
+export class LocationController extends CoreController<LocationControllerRequirements, LocationDatamapperRequirements> {
+  constructor(datamapper: LocationControllerRequirements["datamapper"]) {
+    super(datamapper);
+
+    this.datamapper = datamapper;
+  }
+
+  update = async (req: Request, res: Response): Promise<void> => {
+    const id: number = parseInt(req.params.id);
+
+    if (!id) {
+      throw new BadRequestError("This id doesn't exist");
+    }
+
+    let { zone, alley, position, lvl, lvl_position, location_type_name, location_blockage_name }: Partial<LocationDatamapperRequirements["data"]> = req.body;
+
+    const locationToUpdate = await this.datamapper.findByPk(id);
+
+    if (!locationToUpdate) {
+      console.log("Salut poto")
+      throw new NotFoundError();
+    }
+
+    if (locationToUpdate.version === undefined) {
+      throw new BadRequestError("Version information is missing for the product");
+    }
+    
+    const currentVersion: number = locationToUpdate.version;
+
+    zone ? zone : zone = locationToUpdate.zone;
+    alley ? alley : alley = locationToUpdate.alley;
+    position ? position : position = locationToUpdate.position;
+    lvl ? lvl : lvl = locationToUpdate.lvl;
+    lvl_position ? lvl_position : lvl_position = locationToUpdate.lvl_position;
+    location_type_name ? location_type_name : location_type_name = locationToUpdate.location_type_name;
+    location_blockage_name ? location_blockage_name : location_blockage_name = locationToUpdate.location_blockage_name;
+
+    const newDataLocation = { 
+      ...locationToUpdate, 
+      zone,
+      alley,
+      position,
+      lvl,
+      lvl_position,
+      location_type_name,
+      location_blockage_name
+    };
+
+    const updatedLocation = await this.datamapper.update(newDataLocation, currentVersion);
+
+    if (!updatedLocation) {
+      throw new DatabaseConnectionError();
+    }
+
+    res.status(200).send(updatedLocation);
+  }
+}
--- structure/app/controllers/index.controllers.ts
+++ structure/app/controllers/index.controllers.ts
@@ -1,6 +1,11 @@
-import { locationTypeDatamapper } from "../datamappers/index.datamappers";
+import { locationDatamapper, locationTypeDatamapper } from "../datamappers/index.datamappers";
+import { LocationController } from "./entityControllers/LocationController";
 import { LocationTypeController } from "./entityControllers/LocationTypeController";
 
 const locationTypeController = new LocationTypeController(locationTypeDatamapper);
+const locationController = new LocationController(locationDatamapper)
 
-export { locationTypeController};
+export { 
+  locationTypeController,
+  locationController
+};
