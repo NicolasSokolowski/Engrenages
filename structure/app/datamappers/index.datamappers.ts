31be83f67534322fc76d2508b154bc8bfa5a3a7a
NicolasSokolowski <sokolowski.nicolas@gmail.com>
2024-07-26 01:39:20 +0200

    build: location datamappers & interfaces

--- structure/app/datamappers/index.datamappers.ts
+++ structure/app/datamappers/index.datamappers.ts
@@ -1,5 +1,10 @@
+import { LocationDatamapper } from "./entityDatamappers/LocationDatamapper";
 import { LocationTypeDatamapper } from "./entityDatamappers/LocationTypeDatamapper";
 
 const locationTypeDatamapper = new LocationTypeDatamapper();
+const locationDatamapper = new LocationDatamapper();
 
-export { locationTypeDatamapper};
+export { 
+  locationTypeDatamapper,
+  locationDatamapper
+};
---
+++ structure/app/datamappers/interfaces/LocationDatamapperRequirements.ts
@@ -0,0 +1,16 @@
+import { EntityDatamapperRequirements, TableNames } from "@zencorp/engrenages";
+
+export interface LocationDatamapperRequirements extends EntityDatamapperRequirements {
+  readonly tableName: TableNames;
+  data: {
+    id?: number;
+    zone: string;
+    alley: string;
+    position: string;
+    lvl: string;
+    lvl_position: string;
+    location_type_name: string;
+    location_blockage_name?: string;
+    version?: number;
+  }
+}
