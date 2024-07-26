7568f1cec7999ca1bfb3731baad5acf2db46014e
NicolasSokolowski <sokolowski.nicolas@gmail.com>
2024-07-26 22:34:01 +0200

    build: validations schemas

---
+++ structure/app/validation/index.schemas.ts
@@ -0,0 +1,15 @@
+import blockageCreateSchema from "./schemas/blockage.create.schema";
+import blockageUpdateSchema from "./schemas/blockage.update.schema";
+import locationCreateSchema from "./schemas/location.create.schema";
+import locationUpdateSchema from "./schemas/location.update.schema";
+import typeCreateSchema from "./schemas/type.create.schema";
+import typeUpdateSchema from "./schemas/type.update.schema";
+
+export {
+  blockageCreateSchema,
+  blockageUpdateSchema,
+  locationCreateSchema,
+  locationUpdateSchema,
+  typeCreateSchema,
+  typeUpdateSchema
+};
