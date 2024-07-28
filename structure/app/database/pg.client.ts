ced4e89d2bc239e4c2931e840a1af35030734d4e
NicolasSokolowski <sokolowski.nicolas@gmail.com>
2024-07-29 00:54:43 +0200

    dev: dependencies & config files

--- structure/app/database/pg.client.ts
+++ structure/app/database/pg.client.ts
@@ -2,8 +2,9 @@ import "dotenv/config";
 import { Pool } from "pg";
 
 const poolConfig = {
-  connectionString: `${process.env.PG_URI}`
-}
+  connectionString: 
+    `postgres://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@${process.env.POSTGRES_HOST || "localhost"}:${process.env.POSTGRES_PORT ? parseInt(process.env.POSTGRES_PORT, 10) : 5432}/${process.env.POSTGRES_TEST_DATABASE}`
+};
 
 const pool = new Pool(poolConfig);
 
