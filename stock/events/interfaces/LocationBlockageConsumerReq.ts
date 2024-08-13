a13896662103fb9862d1b2c7652c29a7e68100d9
NicolasSokolowski <sokolowski.nicolas@gmail.com>
2024-08-14 01:46:17 +0200

    build: location type and blockage consummer interfaces

---
+++ stock/events/interfaces/LocationBlockageConsumerReq.ts
@@ -0,0 +1,6 @@
+import { ConsumerReq } from "@zencorp/engrenages";
+import { LocationBlockageDatamapperRequirements } from "../../app/datamappers/interfaces/LocationBlockageDatamapperRequirements";
+
+export interface LocationBlockageConsumerReq extends ConsumerReq {
+  data: LocationBlockageDatamapperRequirements["data"];
+}
