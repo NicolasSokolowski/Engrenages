ba25f2055c214df205ceacb1feb256cb1d6525db
NicolasSokolowski <sokolowski.nicolas@gmail.com>
2024-08-14 01:36:40 +0200

    build: publishers interfaces

---
+++ structure/events/interfaces/LocationTypePublisherReq.ts
@@ -0,0 +1,6 @@
+import { PublisherReq } from "@zencorp/engrenages";
+import { LocationTypeDatamapperRequirements } from "../../app/datamappers/interfaces/LocationTypeDatamapperRequirements";
+
+export interface LocationTypePublisherReq extends PublisherReq {
+  data: LocationTypeDatamapperRequirements["data"];
+}
