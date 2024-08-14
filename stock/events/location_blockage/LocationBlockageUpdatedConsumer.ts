60cc78da651bdb2ae34c67ed6b96c67e6695fd31
NicolasSokolowski <sokolowski.nicolas@gmail.com>
2024-08-14 19:27:33 +0200

    build: location blockage update & delete consumers

---
+++ stock/events/location_blockage/LocationBlockageUpdatedConsumer.ts
@@ -0,0 +1,50 @@
+import { CoreConsumer, RedisManager, RoutingKeys } from "@zencorp/engrenages";
+import { LocationBlockageConsumerReq } from "../interfaces/LocationBlockageConsumerReq";
+import { Channel, ConsumeMessage } from "amqplib";
+import { locationBlockageController } from "../../app/controllers/index.controllers";
+
+export class LocationBlockageUpdatedConsumer extends CoreConsumer<LocationBlockageConsumerReq> {
+  readonly routingKey = RoutingKeys.LocationBlockageUpdated;
+  queue = "blockageUpdateQueue";
+
+  constructor(channel: Channel, exchange: string) {
+    super(channel, exchange);
+  }
+
+  async consume(): Promise<void> {
+    const queue = await this.setupQueue();
+
+    this.channel.consume(queue, async (msg: ConsumeMessage | null) => {
+      if (msg !== null) {
+        try {
+          const data = JSON.parse(msg.content.toString());
+          console.log(`Received message from ${this.exchange} using routing key: ${this.routingKey}`);
+          const currentVersion = data.version -1;
+
+          const updatedItem = await locationBlockageController.datamapper.update(data, currentVersion);
+
+          if (!process.env.REDIS_HOST) {
+            throw new Error("Redis host must be set")
+          }
+
+          console.log("Location blockage updated successfully");
+
+          const redis = RedisManager.getCmdInstance(process.env.REDIS_HOST, 6379);
+          await redis.connect();
+
+          if (updatedItem) {
+            await redis.addResponse({ eventID: data.eventID, success: true });
+          } else if (!updatedItem ) {
+            await redis.addResponse({ eventID: data.eventID, success: false });
+          }
+
+          this.channel.ack(msg);
+        } catch (err) {
+          console.error(err);
+        }
+      } else {
+        console.warn("Received null message");
+      }
+    })
+  }
+}
