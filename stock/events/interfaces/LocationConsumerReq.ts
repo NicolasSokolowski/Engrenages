b3215049dcb49a6ffcf4413cda391954cfdb40bf
NicolasSokolowski <sokolowski.nicolas@gmail.com>
2024-08-14 22:32:57 +0200

    build: location consumer interface

---
+++ stock/events/interfaces/LocationConsumerReq.ts
@@ -0,0 +1,6 @@
+import { ConsumerReq } from "@zencorp/engrenages";
+import { LocationDatamapperRequirements } from "../../app/datamappers/interfaces/LocationDatamapperRequirements";
+
+export interface LocationConsumerReq extends ConsumerReq {
+  data: LocationDatamapperRequirements["data"];
+}
