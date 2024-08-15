b1bee16d60ff553704bf469a8d02ceb2a5b2a28c
NicolasSokolowski <sokolowski.nicolas@gmail.com>
2024-08-16 01:20:28 +0200

    index + config files

--- product/index.ts
+++ product/index.ts
@@ -1,5 +1,6 @@
 import { app } from "./app/index.app";
 import { pool } from "./app/database/pg.client";
+import { RabbitmqManager, RedisManager } from "@zencorp/engrenages";
 
 const start = async () => {
   pool.query('SELECT 1;', (err: Error, res: any) => {
@@ -10,10 +11,37 @@ const start = async () => {
       console.log('Connected to the database!');
     }
   });
-  
-  app.listen(3000, () => {
-    console.log("Listening on port 3000");
-  });
+
+  try {
+    const rabbitMQ = await RabbitmqManager.getInstance(`amqp://${process.env.RABBITMQ_USERNAME}:${process.env.RABBITMQ_PASSWORD}@${process.env.RABBITMQ_HOST}`);
+    console.log("Connected to RabbitMQ");
+
+    if (!process.env.REDIS_HOST) {
+      throw new Error("Redis host must be set");
+    }
+
+    const redis = new RedisManager(process.env.REDIS_HOST, 6379);
+    await redis.connect();
+    console.log("Connected to Redis");
+    
+    process.on("SIGINT", async () => {
+      await rabbitMQ.close();
+      process.exit(0);
+    });
+
+    process.on("SIGTERM", async () => {
+      await rabbitMQ.close();
+      process.exit(0);
+    });
+    
+    app.listen(3000, () => {
+      console.log("Listening on port 3000");
+    });
+
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
 };
 
 start();
