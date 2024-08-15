8a7515e55746aecdd69dbe841e4da1fe7a7d382c
NicolasSokolowski <sokolowski.nicolas@gmail.com>
2024-08-15 17:59:24 +0200

    fix: added CONSTRAINTS on table location

--- structure/migrations/deploy/tables_creation.sql
+++ structure/migrations/deploy/tables_creation.sql
@@ -31,7 +31,7 @@ CREATE TABLE "location" (
   "lvl" CHAR(1) NOT NULL,
   "lvl_position" CHAR(2) NOT NULL,
   "location" CHAR(15) NOT NULL UNIQUE,
-  "location_type_name" VARCHAR(5) NOT NULL REFERENCES location_type("name"),
+  "location_type_name" VARCHAR(5) NOT NULL REFERENCES location_type("name") ON UPDATE CASCADE ON DELETE CASCADE,
   "location_blockage_name" CHAR(3) DEFAULT NULL REFERENCES location_blockage_type("name"),
   "version" INT DEFAULT 0,
   "created_at" TIMESTAMPTZ NOT NULL DEFAULT NOW(),
